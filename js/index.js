4b988fdb72f460ea873f08f9896ade279caf925c
MadhurimaNayak <124715194+MadhurimaNayak@users.noreply.github.com>
2024-06-09 12:53:37 +0530

    Changes to animation

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     var $lefttoright = $(".left-to-right");
-    var $name = $lefttoright.find(".name");
+    var $name = $lefttoright.find("div.name");
     var $clonedname = $name.clone();
     var nameWidth = 10;
 
     $name.find("h2").each(function (i) {
-        nameWidth += $(this).outerWidth(true);
+        nameWidth += $(this,i).outerWidth(true);
     });
 
     //var endPos = $lefttoright.width() - nameWidth;
@@ -16,16 +16,16 @@ $(document).ready(function () {
 
     $clonedname.addClass("cloned").appendTo($lefttoright);
 
-    var infinite = new TimelineMax({ paused: true });
+    var infinite = new TimelineMax({repeat: -1, paused: true });
     var time = 40;
 
     infinite
-        .fromTo($name, time, { rotation: 0.01, x: 0 }, { force3D: true, x: -nameWidth, ease: Linear.easeNone })
-        .fromTo($clonedname, time, { rotation: 0.01, x: nameWidth }, { force3D: true, x: 0, ease: Linear.easeNone }, 0)
-        .to($name, time, { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone })
-        .to($clonedname, time, { force3D: true, rotation: 0.01, x: nameWidth, ease: Linear.easeNone }, 0)
+        .fromTo($name, time, {rotation: 0.01, x: 0}, {force3D: true, x: -nameWidth, ease: Linear.easeNone}, 0)
+        .fromTo($clonedname, time, {rotation: 0.01, x: nameWidth}, {force3D: true, x: 0, ease: Linear.easeNone}, 0)
+        .set($name, {force3D: true, rotation: 0.01, x: nameWidth})
+        .to($clonedname, time, {force3D: true, rotation: 0.01, x: -nameWidth, ease: Linear.easeNone}, time)
+        .to($name, time, {force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone}, time)
         .progress(1).progress(0)
-        .repeat(-1)
         .play();
 
     $lefttoright.on("mouseenter", function () {
