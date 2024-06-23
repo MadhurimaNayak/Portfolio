0c26c6c5cbf53096b7459c9cc6bfc9d0672bbbd5
MadhurimaNayak <124715194+MadhurimaNayak@users.noreply.github.com>
2024-06-24 00:00:33 +0530

    deployment animation issue

--- js/name-animation.js
+++ js/name-animation.js
@@ -1,11 +1,11 @@
-$(window).ready(function () {
+window.onload = function() {
     var $lefttoright = $(".left-to-right");
     var $name = $lefttoright.find("ul.name");
     var $clonedname = $name.clone();
     var nameWidth = 10;
 
     $name.find("li").each(function (i) {
-        nameWidth += $(this,i).outerWidth(true);
+        nameWidth += $(this, i).outerWidth(true);
     });
 
     //var endPos = $lefttoright.width() - nameWidth;
@@ -16,18 +16,17 @@ $(window).ready(function () {
 
     $clonedname.addClass("cloned").appendTo($lefttoright);
 
-    var infinite = new TimelineMax({repeat: -1, paused: true });
+    var infinite = new TimelineMax({ repeat: -1, paused: true });
     var time = 40;
 
     infinite
-        .fromTo($name, time, {rotation: 0.01, x: 0}, {force3D: true, x: -nameWidth, ease: Linear.easeNone}, 0)
-        .fromTo($clonedname, time, {rotation: 0.01, x: nameWidth}, {force3D: true, x: 0, ease: Linear.easeNone}, 0)
-        .set($name, {force3D: true, rotation: 0.01, x: nameWidth})
-        .to($clonedname, time, {force3D: true, rotation: 0.01, x: -nameWidth, ease: Linear.easeNone}, time)
-        .to($name, time, {force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone}, time)
+        .fromTo($name, time, { rotation: 0.01, x: 0 }, { force3D: true, x: -nameWidth, ease: Linear.easeNone }, 0)
+        .fromTo($clonedname, time, { rotation: 0.01, x: nameWidth }, { force3D: true, x: 0, ease: Linear.easeNone }, 0)
+        .set($name, { force3D: true, rotation: 0.01, x: nameWidth })
+        .to($clonedname, time, { force3D: true, rotation: 0.01, x: -nameWidth, ease: Linear.easeNone }, time)
+        .to($name, time, { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone }, time)
         .progress(1).progress(0)
         .play();
 
     $lefttoright.css('visibility', 'visible');
-});
-
+};
