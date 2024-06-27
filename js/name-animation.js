5434d7a854fe7d207b6315ca16fcb99c421b0770
MadhurimaNayak <124715194+MadhurimaNayak@users.noreply.github.com>
2024-06-27 15:08:22 +0530

    Small changes in sections, added footer, imporved animation loading

--- js/name-animation.js
+++ js/name-animation.js
@@ -29,4 +29,41 @@ window.onload = function() {
         .play();
 
     $lefttoright.css('visibility', 'visible');
+
+    var $righttoleft = $(".right-to-left");
+    var $desc = $righttoleft.find("ul.desc");
+    var $cloneddesc = $desc.clone();
+    var descWidth = 10;
+
+    $desc.find("li").each(function (i) {
+        descWidth += $(this,i).outerWidth(true);
+    });
+
+    //var endPos = $righttoleft.width() - descWidth;
+
+    $desc.add($cloneddesc).css({
+        "width": descWidth + "px"
+    });
+
+    $cloneddesc.addClass("cloned").appendTo($righttoleft);
+
+    var infinite = new TimelineMax({repeat: -1, paused: true });
+    var time = 40;
+
+    infinite
+        .fromTo($desc, time, {rotation: 0.01, x: 0}, {force3D: true, x: -descWidth, ease: Linear.easeNone}, 0)
+        .fromTo($cloneddesc, time, {rotation: 0.01, x: descWidth}, {force3D: true, x: 0, ease: Linear.easeNone}, 0)
+        .set($desc, {force3D: true, rotation: 0.01, x: descWidth})
+        .to($cloneddesc, time, {force3D: true, rotation: 0.01, x: -descWidth, ease: Linear.easeNone}, time)
+        .to($desc, time, {force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone}, time)
+        .progress(1).progress(0)
+        .play();
+
+    $righttoleft.on("mouseenter", function () {
+        infinite.pause();
+    }).on("mouseleave", function () {
+        infinite.play();
+    });
+
+    $righttoleft.css('visibility', 'visible');
 };
